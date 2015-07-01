ed86ba088f394b5bf1467acb3d9508fd868d3fa6
Jenny Jensen <jlcjensen@gmail.com>
2015-07-01 15:57:13 -0400

    tournament.py: All tests pass, adding back in swissPairings to finish this up.
    tournament.sql: changed name data type to text from int

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -61,12 +61,12 @@ def registerPlayer(name):
 
     cursor = DB.cursor()
 
-    cursor.execute('INSERT INTO Players (NAME, WINS, LOSSES) values (\'' + name + '\',0,0);')
+    cursor.execute('INSERT INTO Players (NAME, WINS, LOSSES) values (%s,0,0);', (name,))
 
     DB.commit()
 
     DB.close()
-#5
+#5/6
 def playerStandings():
     #Returns a list of the players and their win records, sorted by wins.
 
@@ -90,7 +90,7 @@ def playerStandings():
     DB.close()
     
     return playersarray
-#6
+#7
 def reportMatch(winner, loser):
     #Records the outcome of a single match between two players.
 
@@ -101,26 +101,92 @@ def reportMatch(winner, loser):
 
     cursor = DB.cursor()
 
-    cursor.execute('INSERT INTO Matches (WinnerID, LoserID) values (%d,%d);', (winner,loser))
+    cursor.execute('INSERT INTO Matches (WinnerID, LoserID) values (%s,%s);', (winner,loser,))
+
     DB.commit()
 
+    cursor.execute('SELECT PID, WINS, LOSSES FROM Players WHERE PID = %s OR PID = %s;',(winner,loser,))
+    rows = cursor.fetchall()
+
+    #check returned PIDs against winner and loser IDs
+    if rows[0][0] == winner and rows[1][0] == loser:
+        wins = rows[0][1] + 1
+        cursor.execute('UPDATE Players SET WINS = %s WHERE PID = %s;',(wins,winner,))
+        DB.commit()
+
+        losses = rows[1][2] + 1
+        cursor.execute('UPDATE Players SET LOSSES = %s WHERE PID = %s;',(losses,loser,))
+        DB.commit()
+    else:
+        wins = rows[1][1] + 1
+        cursor.execute('UPDATE Players SET WINS = %s WHERE PID = %s;',(wins,winner,))
+        DB.commit()
+
+        losses = rows[0][2] + 1
+        cursor.execute('UPDATE Players SET LOSSES = %s WHERE PID = %s;',(losses,loser,))
+        DB.commit()
+
     DB.close()
- 
+#8
+def swissPairings():
+    #Returns a list of pairs of players for the next round of a match.
+  
+    #Assuming that there are an even number of players registered, each player
+    #appears exactly once in the pairings.  Each player is paired with another
+    #player with an equal or nearly-equal win record, that is, a player adjacent
+    #to him or her in the standings.
+  
+    #Returns:
+    #  A list of tuples, each of which contains (id1, name1, id2, name2)
+    #    id1: the first player's unique id
+    #    name1: the first player's name
+    #    id2: the second player's unique id
+    #    name2: the second player's name
+
+    #enumerate() may be helpful
+
+    DB = DB.connect()
+    cursor = DB.cursor()
+    playersarray = []
+    matchesarray = []
+    roundarray = []
+
+    #get player info from players, and store in an array
+    cursor.execute('SELECT PID,NAME,WINS FROM Players ORDER BY WINS DESC')
+    playersarray = cursor.fetchall()
+
+    #you can manipulate lists easier than tuples, fetchall returns list of tuples
+
+    count = 0
+    while 
 
 
+    #get previous matches, and store in array
+    cursor.execute('SELECT * FROM Matches');
+    matchesarray = cursor.fetchall();
+
+    #randomize players and return matches (for help: http://stackoverflow.com/questions/7225906/forming-random-pairs-from-a-list-sort-of)
+    def player_matches (playersarray, matchesarray):
+        matches = list(matchesarray);
+
+        #where the players' wins are the same, put in separate arrays
+        #go through the arrays of players one at a time, randomize, check, re-randomize if needed, else push to roundarray
 
 
+        valid_match = false
 
+        while not valid_match:
+            random.shuffle(players)
+    
+    #function
+    #check new matches to matches table
+    #if matches have been played, return false
+    #/function
 
+    #if false, re-randomize
+    #repeat until matches are set
+    
+    #once matches are set, send PID1, Name1, PID2, Name2 to array, and commit to matches and player tables
+    #return array
 
-
-
-
-
-
-
-
-
-
-
-
+    return roundarray;
